b8040d2a0c0db782d232fa6e1d986bb5370d9654
unknown <raymondtanzijian@gmail.com>
2026-09-07 16:26:24 +0800

    add

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/config/config.php';
 
 if (!isset($_SESSION['user'])) {
-    header('Location: /auth/login.php');
+    header('Location: /tnb-meter-system/auth/login.php');
     exit;
 }
 
